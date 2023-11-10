96cb725704b4c7d725b58fd590786a3bfb0d99cd
LEIYAO123 <lei@rushable.io>
2023-11-10 20:31:17 +0800

    feat: 完成 gh-pages 调试

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
-import Unocss from "unocss/vite";
+import Unocss from 'unocss/vite'
 
-import path from "path";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss({})],
+  plugins: [vue(), Unocss({}), legacy({ targets: ['defaults', 'not IE 11'] })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  base: '/xinyun-admin/',
+})
